f29c756e7f150c3fbde409525b2e6b486f0fdca2
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-12-19 22:02:44 +0300

    added reviews table

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -8,10 +8,21 @@ CURSOR = CONN.cursor()
 def database():
     CURSOR.execute('''
 CREATE TABLES IF NOT EXISTS heroes (
-                   id INTERGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL
-                   power TEXT NOT NULL
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                   power TEXT NOT NULL,
                    origin TEXT NOT NULL
                    )
                    ''')
-    CURSOR.execute()
+    
+    CURSOR.execute('''
+CREATES TABLES IF NOT EXISTS review (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   hero_id INTEGER NOT NULL,
+                   rating INTERG NOT NULL CHECK(rating between 1 and 10),
+                   FOREIGN KEY (hero_id) refrences heroes(id)
+                   )
+                   ''')
+    
+CONN.commit()
+CONN.close()
