ffae0961fefe156df24fb493f99db4fa5239ad3f
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-12-19 21:55:54 +0300

    heroes table added

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,17 @@
 import sqlite3
 
-CONN = sqlite3.connect('company.db')
+DATABASE_FILE = 'database.db'
+
+CONN = sqlite3.connect(DATABASE_FILE)
 CURSOR = CONN.cursor()
+
+def database():
+    CURSOR.execute('''
+CREATE TABLES IF NOT EXISTS heroes (
+                   id INTERGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL
+                   power TEXT NOT NULL
+                   origin TEXT NOT NULL
+                   )
+                   ''')
+    CURSOR.execute()
