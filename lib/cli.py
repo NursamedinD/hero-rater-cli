bd11939ebfc8abd38566f5240ac4da2a526313f9
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-12-20 12:43:02 +0300

    added hero deletion function

--- lib/cli.py
+++ lib/cli.py
@@ -13,9 +13,10 @@ def main_menu():
     print("2. View Heroes list")
     print("3. Add a review for a hero")
     print("4. View all reviews for heroes")
-    print("5. Delete a review")
-    print("6. Exit")
-    return input("Type (1-6) to choose an option: ")
+    print("5. Delete a Hero")
+    print("6. Delete a Review")
+    print("7. Exit")
+    return input("Type (1-7) to choose an option: ")
 
 def add_hero():
     print("\n -- Add a new Hero --")
@@ -56,12 +57,25 @@ def view_reviews():
     else:
         print(f"No reviews found for Hero ID {hero_id}.")
 
+def delete_hero():
+    print("\n --Delete a Hero")
+    hero_id = input("Enter Hero ID to view Hero: ")
+    heroes = Hero.get_all_heroes(hero_id)
+    if heroes:
+        print(f"\n Heroes for Hero ID {hero_id}: ")
+        for hero in heroes:
+            hero_id = input("\n Enter Hero ID to delete: ")
+            Hero.delete_hero(hero_id)
+            break
+        else:
+            print(f"No Heroes found for Hero ID {hero_id}: ")
+
 def delete_review():
     print("\n -- Delete a Review --")
     hero_id = input("Enter Hero ID to see their reviews: ")
     reviews = Review.get_reviews_for_hero(hero_id)
     if reviews:
-        print(f"\nReviews for Hero ID {hero_id}:")
+        print(f"\n Reviews for Hero ID {hero_id}:")
         for review in reviews:
             print(f"Review ID: {review.id}, Rating: {review.rating}")
         review_id = input("\nEnter Review ID to delete: ")
@@ -73,7 +87,7 @@ def delete_review():
 
 def hero_cli():
     while True:
-        choice = input("Type (1-6) to choose an option: ")
+        choice = main_menu()
         if choice == "1":
             add_hero()
         elif choice == "2":
@@ -83,8 +97,10 @@ def hero_cli():
         elif choice == "4":
             view_reviews()
         elif choice == "5":
-            delete_review()
+            delete_hero()
         elif choice == "6":
+            delete_review()
+        elif choice == "7":
             print("Exiting...see you next time!")
             break
         else:
